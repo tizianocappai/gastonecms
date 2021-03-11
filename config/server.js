a5d3329f5eca6c3e1d44780f17670dcf810440d2
tiziano.cappai <tiziano.cappai@accenture.com>
2021-03-11 17:32:56 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '785ad600834c3470810e8191c2efecab'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '867f2518-cc9e-49e1-92d0-f1ee48af79d8'
+};
